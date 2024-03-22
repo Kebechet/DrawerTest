fc1913eb1cf47dc76ea0cbee08ca52911d9c27ff
Kebechet <samosidor@gmail.com>
2024-03-22 09:43:33 +0100

    Add project files.

---
+++ DrawerTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DrawerTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
